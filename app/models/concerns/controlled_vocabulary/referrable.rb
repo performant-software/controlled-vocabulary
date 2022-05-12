d9f2d2fa5ac6cf142c36a7cb5869552784fba1f7
Derek Leadbetter <dleadbetter55@gmail.com>
2022-05-12 08:58:47 -0400

    Updating Referrable concern naming convention; Adding ReferrableSerializer concern

--- app/models/concerns/controlled_vocabulary/referrable.rb
+++ app/models/concerns/controlled_vocabulary/referrable.rb
@@ -2,24 +2,39 @@ module ControlledVocabulary
   module Referrable
     extend ActiveSupport::Concern
 
+    class_methods do
+      def references(key, options = {})
+        # Setup the has_many or has_one relationship
+        relationship = options[:multiple] == true ? :has_many : :has_one
+        self.send(relationship, key.to_sym, -> { where(key: key) }, as: :referrable, dependent: :destroy, class_name: 'ControlledVocabulary::Reference')
+
+        # Nested attributes
+        self.send(:accepts_nested_attributes_for, key.to_sym, allow_destroy: true)
+
+        # Resourceable parameters
+        self.send(:allow_params, "#{key}_attributes".to_sym => [:id, :reference_code_id, :key, :_destroy])
+
+        @references ||= {}
+        @references[key] = options
+      end
+
+      # Returns true if the passed attribute is referrable
+      def is_referrable?(attribute)
+        (@references || {}).keys.include?(attribute)
+      end
+
+      # Returns true if the passed attribute allows multiple values
+      def allow_multiple?(attribute)
+        ((@references || {})[attribute] || {})[:multiple] == true
+      end
+    end
+
     included do
       # Relationships
       has_many :references, as: :referrable, dependent: :destroy, class_name: 'ControlledVocabulary::Reference'
 
-      # Nested Attributes
-      accepts_nested_attributes_for :references, allow_destroy: true
-
-      # Resourceable parameters
-      allow_params references_attributes: [:id, :reference_code_id, :key, :_destroy]
-
       # Callbacks
       before_save :validate_references
-
-      def self.references(*references)
-        @references ||= []
-        @references << references if references.present?
-        @references
-      end
 
       def self.with_reference(reference_key, value)
         self.where(
@@ -36,14 +51,17 @@ module ControlledVocabulary
       private
 
       def validate_references
+        return if @references.nil? || @references.empty?
+
         # Find any references with invalid keys
-        keys = self.class.references.map{ |k, options| k }
         self.references.each do |reference|
-          next if keys.include?(reference.key.to_sym)
+          next if @references.keys.include?(reference.key.to_sym)
           errors.add(:references, "Invalid reference: #{reference.key}")
         end
 
-        self.class.references.each do |key, options = {}|
+        @references.keys.each do |key|
+          options = @references[key] || {}
+
           records = self.references.select{ |r| r.key.to_sym == key }
 
           # Find any required references that are not present
---
+++ app/serializers/concerns/controlled_vocabulary/referrable_serializer.rb
@@ -0,0 +1,33 @@
+module ControlledVocabulary
+  module ReferrableSerializer
+    extend ActiveSupport::Concern
+
+    included do
+      def extract_value(serialized, item, attribute)
+        super unless item.class.is_referrable?(attribute)
+        return unless item.class.is_referrable?(attribute)
+
+        if item.class.allow_multiple?(attribute)
+          extract_has_many serialized, item, attribute
+        else
+          extract_has_one serialized, item, attribute
+        end
+      end
+
+      private
+
+      def extract_has_many(serialized, item, attribute)
+        serialized[attribute] = []
+
+        item.send(attribute)&.each do |reference|
+          serialized[attribute] << reference.reference_code&.name
+        end
+      end
+
+      def extract_has_one(serialized, item, attribute)
+        reference = item.send(attribute)
+        serialized[attribute] = reference.reference_code&.name
+      end
+    end
+  end
+end
