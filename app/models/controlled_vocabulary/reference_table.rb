2d69ae6feaadff8c65c6c35ff8501f10f9aaa7d4
Derek Leadbetter <dleadbetter55@gmail.com>
2022-05-17 07:31:09 -0400

    Updating reference_table to allow nested attributes for reference codes

--- app/models/controlled_vocabulary/reference_table.rb
+++ app/models/controlled_vocabulary/reference_table.rb
@@ -1,5 +1,12 @@
 module ControlledVocabulary
   class ReferenceTable < ApplicationRecord
+    # Relationships
     has_many :reference_codes, dependent: :destroy
+
+    # Nested attributes
+    accepts_nested_attributes_for :reference_codes, allow_destroy: true
+
+    # Resourceable parameters
+    allow_params :name, :key, reference_codes_attributes: [:id, :name, :_destroy]
   end
 end
