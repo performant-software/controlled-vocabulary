3bc7f9049cab45983d55b610837f0997b88f3521
Derek Leadbetter <dleadbetter55@gmail.com>
2022-05-17 07:29:45 -0400

    Updating base_controller to prefix attributes with scoped table name.

--- app/controllers/controlled_vocabulary/base_controller.rb
+++ app/controllers/controlled_vocabulary/base_controller.rb
@@ -1,11 +1,21 @@
 module ControlledVocabulary
   class BaseController < ControlledVocabulary.config.base_controller_class.constantize
+    def self.search_attributes(*attrs)
+      super *attrs.map{ |attr| attr.is_a?(Symbol) ? "#{table_name}.#{attr}" : attr }
+    end
+
     def item_class
       "ControlledVocabulary::#{controller_name.singularize.classify}".constantize
     end
 
     def serializer_class
       "ControlledVocabulary::#{"#{controller_name}_serializer".classify}".constantize
+    end
+
+    private
+
+    def self.table_name
+      "ControlledVocabulary::#{controller_name.singularize.classify}".constantize.table_name
     end
   end
 end
