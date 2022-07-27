4cca7d9ffff8847932e915abe4846c4dd8cbe151
Derek Leadbetter <dleadbetter55@gmail.com>
2022-07-27 09:58:21 -0400

    UDCSL #28 - Adding /api/reference_tables/find_by_key API end point

--- app/controllers/controlled_vocabulary/reference_tables_controller.rb
+++ app/controllers/controlled_vocabulary/reference_tables_controller.rb
@@ -1,5 +1,22 @@
 module ControlledVocabulary
   class ReferenceTablesController < BaseController
+    # Search attributes
     search_attributes :name
+
+    def find_by_key
+      render json: { errors: [I18n.t('errors.parameter_required', name: 'key')] }, status: :unprocessable_entity and return unless params[:key]
+
+      query = base_query
+      query = build_query(query)
+
+      item = query.find_by(key: params[:key])
+      item_name = param_name.to_sym
+
+      item = prepare_item(item)
+      preloads(item)
+
+      serializer = serializer_class.new(current_user)
+      render json: { item_name => serializer.render_show(item) }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ControlledVocabulary::Engine.routes.draw do
   resources :reference_codes, only: :index
-  resources :reference_tables
+  resources :reference_tables do
+    get :find_by_key, on: :collection
+  end
 end
