f4f07d546e57716c9ecefcfcdbae47c7fd5b970e
Derek Leadbetter <dleadbetter55@gmail.com>
2022-08-18 13:03:32 -0400

    UDCSL #51 - Updating references_serializer to include reference code object

--- app/serializers/controlled_vocabulary/references_serializer.rb
+++ app/serializers/controlled_vocabulary/references_serializer.rb
@@ -1,6 +1,6 @@
 module ControlledVocabulary
   class ReferencesSerializer < BaseSerializer
-    index_attributes :id, :reference_code_id, :key
-    show_attributes :id, :reference_code_id, :key
+    index_attributes :id, :reference_code_id, :key, reference_code: [:id, :name]
+    show_attributes :id, :reference_code_id, :key, reference_code: [:id, :name]
   end
 end
